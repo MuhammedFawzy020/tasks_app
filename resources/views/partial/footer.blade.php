cbea67b66e36eb16e9255f88fe7c75c00415c298
Mm.Mustafa-c <mu7mdmagdy@gmail.com>
2024-08-15 15:18:43 +0300

    remove everything

--- resources/views/partial/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="footer">
-    <div class="container-fluid clearfix">
-        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block" style="color:black !important">Copyright © Convertedin.com 2024</span>
-        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center" style="color:black !important"> Task App To : <a href="https://www.converted.in/" target="_blank" style="color:blue !important">Convertedin</a></span>
-    </div>
-</footer>
